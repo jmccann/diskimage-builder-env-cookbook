f549b6d8019b892e97ae656c1c39ff9715854384
Jacob McCann <jacob.mccann2@target.com>
2015-07-07 14:14:15 -0500

    Version 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.0.0
+* Hotfix installing DIB_SAT_CERT_RPM_URL from rhel-common element
+* Remove initial CentOS 6.x support as it can't work
+
 # 0.2.0
 * Intial CentOS 6.x support
 
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jacob.mccann2@target.com'
 license          'Apache 2.0'
 description      'Installs/Configures diskimage-builder-env'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '1.0.0'
 
 depends 'yum'
 depends 'yum-epel', '0.6.0'

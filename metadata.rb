84cfe7814e75f44c0d34faa1839126f564fe2b53
Jacob McCann <jacob.mccann2@target.com>
2015-07-07 10:57:20 -0500

    Update license to Apache 2.0

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'diskimage-builder-env'
 maintainer       'Jacob McCann'
 maintainer_email 'jacob.mccann2@target.com'
-license          'all_rights'
+license          'Apache 2.0'
 description      'Installs/Configures diskimage-builder-env'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'

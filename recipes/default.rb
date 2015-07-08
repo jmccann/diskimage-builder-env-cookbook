8ec2d0d95b055900c83d3a638cc36d8d48c5df7c
Jacob McCann <jacob.mccann2@target.com>
2015-07-08 14:53:32 -0500

    Need to 'update' /usr/share/rhn/RHNS-CA-CERT for RHEL6
    And it doesn't seem to break RHEL7 to do so either

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,4 +23,5 @@ end
 cookbook_file '/root/.bash_profile'
 
 # Fix installing DIB_SAT_CERT_RPM_URL ... contrib back
+# 'Update' /usr/share/rhn/RHNS-CA-CERT ... contrib back
 cookbook_file '/root/diskimage-builder/elements/rhel-common/pre-install.d/00-rhsm'

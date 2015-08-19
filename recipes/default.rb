35d26d842798c984e755aeb69cb0ad61e29e3172
Jacob McCann <jacob.mccann2@target.com>
2015-08-19 11:19:46 -0500

    Elements workarounds
    * Properly detect init system for RHEL
    * Properly dhcp all interfaces on RHEL
    * Disable RHN services

--- recipes/default.rb
+++ recipes/default.rb
@@ -25,3 +25,15 @@ cookbook_file '/root/.bash_profile'
 # Fix installing DIB_SAT_CERT_RPM_URL ... contrib back
 # 'Update' /usr/share/rhn/RHNS-CA-CERT ... contrib back
 cookbook_file '/root/diskimage-builder/elements/rhel-common/pre-install.d/00-rhsm'
+
+# Enable service with chkconfig if update.rc does not exist ... contrib back
+# Update 'network' service to depend on dhcp-all-interfaces ... contrib back
+cookbook_file '/root/diskimage-builder/elements/dhcp-all-interfaces/install.d/50-dhcp-all-interfaces'
+
+# Disable RHN services
+cookbook_file '/root/diskimage-builder/elements/rhel-common/finalise.d/99-disable-services' do
+  mode 0755
+end
+
+# Properly detect sysv from upstart as RHEL6 has both installed but uses sysv ... contrib fix
+cookbook_file '/root/diskimage-builder/elements/dib-init-system/dib-init-system'
